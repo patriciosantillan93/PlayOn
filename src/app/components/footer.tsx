9529818c9daad18721698bb41d00fbdfc4f44f63
patriciosantillan93 <patricio.santillan93@gmail.com>
2024-10-31 17:35:05 -0400

    Creacion un par de paginas y algunos componentes como footer y header

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,14 @@
+// app/components/Footer.tsx
+
+import React from 'react';
+import styles from './Footer.module.css'; // Import the CSS module
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}> {/* Apply the .footer class */}
+      <p className={styles.footerText}>&copy; {new Date().getFullYear()} Powered by PlayON. All rights reserved.</p> {/* Apply the .footerText class */}
+    </footer>
+  );
+};
+
+export default Footer;
