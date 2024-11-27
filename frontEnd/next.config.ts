835992b3fce2b103f0cba7aa9936a4d05509d6b2
andresbonelli <andresbonellipiano@gmail.com>
2024-11-27 09:23:37 -0300

    undo rewrite path

--- frontEnd/next.config.ts
+++ frontEnd/next.config.ts
@@ -1,16 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/:path*",
-        destination: process.env.NEXT_PUBLIC_API_URL + "/:path*",
-      },
-    ];
-  },
-  output: "standalone",
-
   images: {
     remotePatterns: [
       {
